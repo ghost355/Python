7db7969d1b3da9cc09879fdcb4bac8a859d7f21f
ghost355 <kxx355@gmail.com>
2016-05-05 05:40:15 +0300

    init commit

---
+++ first.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+# by Pavel Pavlov (c) 2016
+# My second  program on Python 3
+
+import sys
+
+try:
+        digits = sys.argv[1]
+        row = 0
+        while row < 7:
+                line = ""
+                column = 0
+                while column < len(digits):
+                        number = int(digits[column])
+                        digit = Digits[number]
+                        line += digit[row] + " "
+                        column += 1
+        print(line)
+        row += 1
+except IndexError:
+        print("usage: bigdigits.py <number>")
+except ValueError as err:
+        print(err, "in", digits)
